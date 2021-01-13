07e5945ff9360051f435ae7f78ea8dfc1addff7b
Cam Tullos <cam@reactium.io>
2021-01-13 09:23:08 -0500

    + Added 'reactium-ui' hook to allow for altering the library when it's registered as a hook component. + published @atomic-reactor/reactium-ui@0.2.6

--- src/app/components/common-ui/reactium-plugin/reactium-hooks.js
+++ src/app/components/common-ui/reactium-plugin/reactium-hooks.js
@@ -1,14 +1,9 @@
 import Reactium from '@atomic-reactor/reactium-sdk-core';
 
 const ReactiumUI = require('../index');
+const priority = Reactium.Enums.priority.lowest;
 
-const pluginInit = async () => {
-    await Reactium.Plugin.register(
-        'ReactiumUI',
-        Reactium.Enums.priority.lowest,
-    );
-
+Reactium.Plugin.register('ReactiumUI', priority).then(() => {
+    Reactium.Hook.runSync('reactium-ui', ReactiumUI);
     Reactium.Component.register('ReactiumUI', ReactiumUI);
-};
-
-pluginInit();
+});
